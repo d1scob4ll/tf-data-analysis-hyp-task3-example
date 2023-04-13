54fae868f181eb5d21cea31809d476cb71829e44
d1scob4ll <130067358+d1scob4ll@users.noreply.github.com>
2023-04-14 00:00:40 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,19 @@
+import scipy.stats as stats
 import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 305011093 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(control_npv: np.array, test_npv: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.1
+    t_stat, p_val = stats.ttest_ind(control_npv, test_npv, equal_var=True)
+
+    if p_val < alpha:
+        t = True
+    else:
+        t = False   
+    return t
